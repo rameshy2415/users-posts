32528cf710ddfa7d6bcd9daa4261c4970178599e
rameshy2415 <rameshy2415@gmail.com>
2024-05-30 23:36:03 +0530

    initial commit

---
+++ src/main/java/com/techdive/users_posts/UsersPostsApplication.java
@@ -0,0 +1,13 @@
+package com.techdive.users_posts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsersPostsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsersPostsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/techdive/users_posts/UsersPostsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techdive.users_posts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsersPostsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
